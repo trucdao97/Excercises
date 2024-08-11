823563d1bad54b379b48c21c786dd5ebdc8b63ec
trucdao97 <trucdao7297@gmail.com>
2024-08-11 21:45:44 +0700

    Excercise_02

---
+++ src/main/javabasic/Excercise_02.java
@@ -0,0 +1,14 @@
+public class Excercise_02 {
+    public static void total2Numbers(int number1, int number2) {
+        System.out.println("Total of 2 numbers = " + (number1 + number2));
+    }
+
+    public static void multiple2Numbers(double number3, double number4) {
+        System.out.println("Multiple of 2 numbers = " + (number3 + number4));
+    }
+
+    public static void main(String[] args) {
+        total2Numbers(10,20);
+        multiple2Numbers(1.5,4.5);
+    }
+}
