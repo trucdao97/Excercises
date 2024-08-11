ae6dfd14423267bbc0271e80d055672d873e8346
trucdao97 <trucdao7297@gmail.com>
2024-08-11 22:42:15 +0700

    Excercise 03

--- src/main/javabasic/Excercise_03.java
+++ src/main/javabasic/Excercise_03.java
@@ -1,2 +1,17 @@
 public class Excercise_03 {
+    public static void compareNumbers(int n) {
+        int number1 = 100;
+        if (n < number1){
+            System.out.println("Input number is less than number1");
+        }else if(n > number1) {
+            System.out.println("Input number is greater than number1");
+        } else {
+            System.out.println("2 numbers is equal");
+        }
+    }
+    public static void main(String[] args) {
+        compareNumbers(10);
+        compareNumbers(500);
+        compareNumbers(100);
+    }
 }
