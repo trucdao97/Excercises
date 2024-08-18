acfe55b5f5249af2337541813fad526c4ee7c07c
trucdao97 <trucdao7297@gmail.com>
2024-08-18 18:36:51 +0700

    Excercise 04: simple for loop

--- src/main/javabasic/forLoopAndArray/Excercise_04_forLoop.java
+++ src/main/javabasic/forLoopAndArray/Excercise_04_forLoop.java
@@ -1,4 +1,9 @@
 package forLoopAndArray;
 
 public class Excercise_04_forLoop {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 50; i = i + 2){
+            System.out.println(i);
+        }
+    }
 }
