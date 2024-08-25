1f84f5a0e1945c6431658d01849ec7121477036f
trucdao97 <trucdao7297@gmail.com>
2024-08-25 16:44:15 +0700

    Excercise 04: simple for loop

--- src/main/javabasic/forLoopAndArray/Excercise_04_forLoop.java
+++ src/main/javabasic/forLoopAndArray/Excercise_04_forLoop.java
@@ -2,8 +2,20 @@ package forLoopAndArray;
 
 public class Excercise_04_forLoop {
     public static void main(String[] args) {
-        for (int i = 0; i <= 50; i = i + 2){
-            System.out.println(i);
+        //Tạo mảng để lưu giá trị chẵn từ 0 đến 50
+        int maxNumber = 50;
+        int array[] = new int[(maxNumber/2)+1];
+        //Tạo vòng lặp FOR để in ra các số chẵn từ 0 đến 50
+        int i;
+        for (i = 0; i <= maxNumber/2; i++) {
+            array[i] = 2*i;
+//            System.out.println("Cac so chan: " + i);
+        }
+
+        //Duyệt mảng để in ra kết quả sau khi nạp vào
+
+        for (int a = 0; a < array.length; a++) {
+            System.out.println(array[a]);
         }
     }
 }
